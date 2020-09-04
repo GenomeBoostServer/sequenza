3607cdf8baec75392dec6a9826af1bd1b92c695c
Francesco Favero <francesco.favero@bric.ku.dk>
2020-09-04 13:20:39 +0200

    stremline the chunk.apply approach, and generalize the function so it can be used for other purpose with minimal arguments changes

---
+++ R/datastats.R
@@ -0,0 +1,190 @@
+splash_table <- function(lis_obj) {
+    lis_obj <- Reduce("c", lis_obj)
+    split(lis_obj, names(lis_obj))
+}
+
+unfold_data <- function(
+    x, f = do_get_gc, args_f = get_gc_defaults, stats = TRUE) {
+
+    data_items <- colnames(x)[3:ncol(x)]
+    xyz_mats <- lapply(data_items, FUN = function(x, stat_obj,
+        f, args) {
+        f(stat_obj[, x], args)
+    }, stat_obj = x, f = f, args = args_f)
+
+    names(xyz_mats) <- data_items
+
+    if (stats) {
+        ord_chrom <- unique(Reduce("c", Reduce("c", x[, "unique"])))
+        stats_chrom <- Reduce("c", x[, "lines"])
+        stats_chrom <- sapply(sequenza:::splash_table(x[, "lines"]), sum)
+        stats_chrom <- stats_chrom[ord_chrom]
+        stats_start <- cumsum(c(1, stats_chrom[-length(stats_chrom)]))
+        stats_end   <- stats_start + stats_chrom - 1
+        stats_chrom <- data.frame(chr = ord_chrom, n_lines = stats_chrom,
+            start = stats_start, end = stats_end)
+
+        c(list(file.metrics = stats_chrom), xyz_mats)
+    } else {
+        xyz_mats
+    }
+}
+
+data_fast_stats <- function(file, col_types = "c--dd----d----",
+    buffer = 33554432, parallel = 2L, stats = TRUE, verbose = TRUE,
+    col_sets = list("normal" = c(2, 4), "tumor" = c(3, 4)),
+    f1 = gc_table, f2 = do_get_gc, args_f1 = list(), args_f2 = get_gc_defaults,
+    msg = NULL) {
+    con <- gzfile(file, "rb")
+
+    suppressWarnings(skip_line <- readLines(con, n = 1))
+    remove(skip_line)
+    parse_chunck <- function(x, col_types,
+        sets = col_sets, f = f1, args_f = args_f1) {
+        x <- read_tsv(file = paste(mstrsplit(x), collapse = "\n"),
+            col_types = col_types, col_names = FALSE,
+            skip = 0, n_max = Inf, progress = FALSE)
+        n_chr <- table(x[, 1])
+        u_chr <- names(n_chr)
+        set_lists <- lapply(sets, f, y = x, args = args_f)
+        if (verbose) {
+            message(".", appendLF = FALSE)
+        }
+        c(list(unique = u_chr, lines = n_chr), set_lists)
+    }
+    if (verbose) {
+        message(msg, appendLF = FALSE)
+    }
+    res <- chunk.apply(
+        input = con, FUN = function(x, col_types, sets, f, args) {
+            parse_chunck(
+                ßx, col_types = col_types, sets = sets, f = f, args)
+        }, col_types = col_types, sets = col_sets, f = f1, args = args_f1,
+        CH.MAX.SIZE = buffer, parallel = parallel)
+    close(con)
+    if (verbose) {
+        message(" done\n")
+    }
+    #res 
+    unfold_data(x = res, f = f2, args_f = args_f2, stats = stats)
+}
+
+
+
+do_ratio <- function(x, y, args) {
+    lm_normal <- args[["lm_normal"]]
+    lm_tumor <- args[["lm_tumor"]]
+    round_r <- args[["round_dr"]]
+    round_b <- args[["round_bf"]]
+    colnames(y)[x] <- names(x)
+    y <- y[y$zyg == "het", ]
+    norm_tumor_depth <- y$tumor /
+        predict(lm_tumor, y$gc)$y
+    norm_normal_depth <- y$normal /
+        predict(lm_normal, y$gc)$y
+    d_r <- round(norm_tumor_depth / norm_normal_depth, round_r)
+    baf <- round(y$baf, round_b)
+
+    lapply(split(c(d_r, d_r), c(baf, 1 - baf)), table)
+}
+
+
+do_get_ratio_baf <- function(x, args) {
+    smooth <- args[["smooth"]]
+    min_times <- args[["min_times"]]
+    grid_size <- args[["grid_size"]]
+    scale_subset <- args[["scale_subset"]]
+    get_baf_ratio(x, smooth = smooth, min_times = min_times,
+            grid_size = grid_size, scale.subset = scale_subset) 
+}
+
+get_baf_ratio <- function(baf_col, smooth = TRUE,
+    min_times = 20, grid_size = 100, scale.subset = 1.5, ...) {
+    sort_char <- function(x) {
+        as.character(sort(as.numeric(x)))
+    }
+    all_ratios <- sequenza:::splash_table(baf_col)
+    all_ratios <- lapply(all_ratios, FUN = function(x) {
+        sapply(sequenza:::splash_table(x), sum)
+    })
+    names_baf <- sort_char(names(all_ratios))
+    all_ratios <- all_ratios[names_baf]
+    names_ratios <- sort_char(unique(Reduce("c", lapply(all_ratios, names))))
+    n <- do.call(rbind, lapply(all_ratios, FUN = function(x, names_ratios) {
+            res <- x[names_ratios]
+            names(res) <- names_ratios
+            res
+        },
+        names_ratios = names_ratios))
+    n[is.na(n)] <- 0
+    if (smooth == TRUE) {
+        part <- sequenza:::gc_data_smooth(list(
+            gc = as.numeric(names_baf), depth = as.numeric(
+                names_ratios), n = n),
+            min_times = min_times, n = grid_size,
+            scale.subset = scale.subset, ...)
+        names(part) <- c("x", "y", "z")
+        part
+    } else {
+        list(baf = as.numeric(names_baf), ratio = as.numeric(
+            names_ratios), n = n)
+    }
+}
+
+
+# zzz <- data_fast_stats(data.file)
+
+# gc.normal.vect <- mean_gc(zzz$normal)
+# gc.tumor.vect <- mean_gc(zzz$tumor)
+
+
+
+# gc_glm_normal <- smooth.spline(data.frame(
+#     gc = as.numeric(names(gc.normal.vect)), depth = gc.normal.vect))
+
+
+# gc_glm_tumor <- smooth.spline(data.frame(
+#     gc = as.numeric(names(gc.tumor.vect)), depth = gc.tumor.vect))
+
+ratio_args <- list(
+    "lm_normal" = gc_glm_normal,
+    "lm_tumor" = gc_glm_tumor,
+    "round_dr" = 1,
+    "round_bf" = 2
+)
+
+get_gc_defaults <- list("smooth" = TRUE,
+    "min_times" = 5, "grid_size" = 250,
+    "scale_subset" = 10)
+
+
+
+# zz2 <- data_fast_stats(data.file, col_types = "---dd--dcd----",
+#     stats = FALSE, verbose = TRUE, col_sets = list("dr" = c(
+#         "normal" = 1, "tumor" = 2, "baf" = 3, "zyg" = 4, "gc" = 5)),
+#     f1 = do_ratio, f2 = do_get_ratio_baf, args_f1 = ratio_args,
+#     msg = "Collecting BAF/raio information")
+
+rs_baf_ratio <- function(dens, n = 100, min_prob = 1) {
+    df_r <- approxfun(density(rep(dens$y, times = colSums(dens$z))))
+    df_b <- approxfun(density(rep(dens$x, times = rowSums(dens$z))))
+    #df_r <- splinefun(x = dens$y, y = colSums(dens$z))
+    #df_b <- splinefun(x = dens$x, y = rowSums(dens$z))
+    bs <- 0:1000 / 2000
+    rs <- 0:2000 / 100
+    p_bs <- df_b(bs)
+    p_rs <- df_r(rs)
+    p_bs[is.na(p_bs)] <- 0
+    p_rs[is.na(p_rs)] <- 0
+    p_bs[p_bs <= 0] <- 0
+    p_rs[p_rs <= 0] <- 0
+
+    sample_b <- sample(bs, n, TRUE, p_bs)
+    sample_r <- sample(rs, n, TRUE, p_rs)
+    res <- cbind(
+        baf = sample_b,
+        ratio = sample_r,
+        l = df_b(
+            sample_b) / max(p_bs) * df_r(sample_r) / max(p_rs))
+    res[res[, 3] >= min_prob, 1:2]
+}
--- R/gcstats.R
+++ R/gcstats.R
@@ -1,61 +1,46 @@
-gc.sample.stats <- function(file, col_types = "c--dd----d----",
-    buffer = 33554432, parallel = 2L, stats = TRUE, smooth = TRUE,
-    min_times = 20, n = 100, scale.subset = 1.5, verbose = TRUE, ...) {
-    con <- gzfile(file, "rb")
+median_gc <- function(gc_list) {
+    apply(gc_list$n, 1, FUN = function(x, w) {
+            weighted.median(x = w, w = x, na.rm = TRUE)
+        },
+        w = gc_list$depth)
+}
 
-    suppressWarnings(skip_line <- readLines(con, n = 1))
-    remove(skip_line)
-    parse_chunck <- function(x, col_types) {
-        x <- read_tsv(file = paste(mstrsplit(x), collapse = "\n"),
-            col_types = col_types, col_names = FALSE,
-            skip = 0, n_max = Inf, progress = FALSE)
-        u_chr <- unique(x[, 1])
-        n_chr <- table(x[, 1])
-        gc1 <- lapply(split(x[, 2], x[, 4]), table)
-        gc2 <- lapply(split(x[, 3], x[, 4]), table)
-        if (verbose) {
-            message(".", appendLF = FALSE)
+mean_gc <- function(gc_list) {
+    apply(gc_list$n, 1, FUN = function(x, w) {
+            weighted.mean(x = w, w = x, na.rm = TRUE)
+        },
+        w = gc_list$depth)
+}
+
+depths_gc <- function(depth_n, depth_t, gc) {
+    gc_nor <- lapply(split(depth_n, gc), table)
+    gc_tum <- lapply(split(depth_t, gc), table)
+    list(gc_nor = gc_nor, gc_tum = gc_tum)
+}
+
+gc_data_smooth <- function(gc_list, min_times = 20, n = 100,
+    scale.subset = 1.5, ...) {
+
+    mengc <- mean_gc(gc_list)
+    medgc <- median_gc(gc_list)
+    max_depth <- round(max(c(mengc, medgc)) * scale.subset, 0)
+
+    comb_depth_gc <- expand.grid(
+        gc = gc_list$gc,
+        depth = gc_list$depth[gc_list$depth <= max_depth])
+
+    expanded <- pbapply(comb_depth_gc, 1, FUN = function(x, n, t) {
+        times <- n[as.character(x[1]), as.character(x[2])]
+        if (times >= t) {
+            t(matrix(rep(x, times = times / t), nrow = 2))
         }
-        list(unique = u_chr, lines = n_chr, gc_nor = gc1, gc_tum = gc2)
-    }
-    if (verbose) {
-        message("Collecting GC information ", appendLF = FALSE)
-    }
-    res <- chunk.apply(input = con, FUN = parse_chunck, col_types = col_types,
-        CH.MAX.SIZE = buffer, parallel = parallel)
-    close(con)
-    if (verbose) {
-        message(" done\n")
-    }
-    unfold_gc(res, stats = TRUE, smooth, min_times,
-        n, scale.subset, ...)
-}
-
-unfold_gc <- function(x, stats = TRUE, smooth = TRUE,
-    min_times = 20, grid_size = 100, scale.subset = 1.5, ...) {
-    gc_norm <- get_gc(x[, "gc_nor"], smooth, min_times,
-        grid_size, scale.subset, ...)
-    gc_tum <- get_gc(x[, "gc_tum"], smooth, min_times,
-        grid_size, scale.subset, ...)
-    if (stats) {
-        ord_chrom <- unique(Reduce("c", Reduce("c", x[, "unique"])))
-        stats_chrom <- Reduce("c", x[, "lines"])
-        stats_chrom <- sapply(splash_table(x[, "lines"]), sum)
-        stats_chrom <- stats_chrom[ord_chrom]
-        stats_start <- cumsum(c(1, stats_chrom[-length(stats_chrom)]))
-        stats_end   <- stats_start + stats_chrom - 1
-        stats_chrom <- data.frame(chr = ord_chrom, n_lines = stats_chrom,
-            start = stats_start, end = stats_end)
-
-        list(file.metrics = stats_chrom, normal = gc_norm, tumor = gc_tum)
-    } else {
-        list(normal = gc_norm, tumor = gc_tum)
-    }
-}
-
-splash_table <- function(lis_obj) {
-    lis_obj <- Reduce("c", lis_obj)
-    split(lis_obj, names(lis_obj))
+    }, n = gc_list$n, t = min_times, ...)
+    expanded <- do.call(rbind, expanded)
+    regrid <- kde2d(expanded[, 1], expanded[, 2], n = n)
+    n_tab <- regrid$z
+    colnames(n_tab) <- as.character(regrid$y)
+    rownames(n_tab) <- as.character(regrid$x)
+    list(gc = regrid$x, depth = regrid$y, n = n_tab)
 }
 
 get_gc <- function(gc_col, smooth = TRUE,
@@ -87,47 +72,33 @@ get_gc <- function(gc_col, smooth = TRUE,
     }
 }
 
-median_gc <- function(gc_list) {
-    apply(gc_list$n, 1, FUN = function(x, w) {
-            weighted.median(x = w, w = x, na.rm = TRUE)
-        },
-        w = gc_list$depth)
-}
+gc.sample.stats <- function(file, col_types = "c--dd----d----",
+    buffer = 33554432, parallel = 2L, stats = TRUE, smooth = TRUE,
+    min_times = 20, n = 100, scale.subset = 1.5, verbose = TRUE, ...) {
 
-mean_gc <- function(gc_list) {
-    apply(gc_list$n, 1, FUN = function(x, w) {
-            weighted.mean(x = w, w = x, na.rm = TRUE)
-        },
-        w = gc_list$depth)
-}
+    gc_table <- function(x, y, args) {
+        lapply(split(y[, x[1]],
+            y[, x[2]]), table)
+    }
 
-depths_gc <- function(depth_n, depth_t, gc) {
-    gc_nor <- lapply(split(depth_n, gc), table)
-    gc_tum <- lapply(split(depth_t, gc), table)
-    list(gc_nor = gc_nor, gc_tum = gc_tum)
-}
+    do_get_gc <- function(x, args) {
+        smooth <- args[["smooth"]]
+        min_times <- args[["min_times"]]
+        grid_size <- args[["grid_size"]]
+        scale_subset <- args[["scale_subset"]]
+        get_gc(x, smooth = smooth, min_times = min_times,
+            grid_size = grid_size, scale.subset = scale_subset)
+    }
 
-gc_data_smooth <- function(gc_list, min_times = 20, n = 100,
-    scale.subset = 1.5, ...) {
+    get_gc_args <- list("smooth" = smooth,
+        "min_times" = min_times, "grid_size" = n,
+        "scale_subset" = scale.subset)
 
-    mengc <- sequenza:::mean_gc(gc_list)
-    medgc <- sequenza:::median_gc(gc_list)
-    max_depth <- round(max(c(mengc, medgc)) * scale.subset, 0)
-
-    comb_depth_gc <- expand.grid(
-        gc = gc_list$gc,
-        depth = gc_list$depth[gc_list$depth <= max_depth])
-
-    expanded <- pbapply(comb_depth_gc, 1, FUN = function(x, n, t) {
-        times <- n[as.character(x[1]), as.character(x[2])]
-        if (times >= t) {
-            t(matrix(rep(x, times = times / t), nrow = 2))
-        }
-    }, n = gc_list$n, t = min_times, ...)
-    expanded <- do.call(rbind, expanded)
-    regrid <- kde2d(expanded[, 1], expanded[, 2], n = n)
-    n_tab <- regrid$z
-    colnames(n_tab) <- as.character(regrid$y)
-    rownames(n_tab) <- as.character(regrid$x)
-    list(gc = regrid$x, depth = regrid$y, n = n_tab)
+    data_fast_stats(file, col_types = col_types,
+        buffer = buffer, parallel = parallel, stats = stats,
+        verbose = verbose,
+        col_sets = list("normal" = c(2, 4), "tumor" = c(3, 4)),
+        f1 = gc_table, f2 = do_get_gc,
+        args_f1 = list(), args_f2 = get_gc_args,
+        msg = "Collecting GC information ")
 }
