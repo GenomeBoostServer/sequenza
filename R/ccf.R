fc14643a603344bc0bf54cefa716a616c0cc1498
Francesco Favero <favero.francesco@bric.ku.dk>
2025-01-29 02:30:29 +0100

    Add function draft to estimarte ccf from the depth ratio

--- R/ccf.R
+++ R/ccf.R
@@ -97,6 +97,98 @@ depth_ratio_ccf_likelihood <- function(x, CCFs, CNn, CNt, CNst, cellularity, plo
         depth = depth, avg.depth.ratio = avg.depth.ratio, normal.ploidy = normal.ploidy)
 }
 
+#' Calculate Cancer Cell Fraction (CCF) per Segment
+#'
+#' This function calculates the Cancer Cell Fraction (CCF) for each segment in the given segments table.
+#'
+#' @param segs_tab A data frame containing segment information with the following columns:
+#'   - chromosome: integer, chromosome number
+#'   - start.pos: integer, start position of the segment
+#'   - end.pos: integer, end position of the segment
+#'   - Bf: numeric, B allele frequency
+#'   - N.BAF: numeric, number of B allele frequency data points
+#'   - sd.BAF: numeric, standard deviation of B allele frequency
+#'   - depth.ratio: numeric, depth ratio
+#'   - N.ratio: numeric, number of depth ratio data points
+#'   - sd.ratio: numeric, standard deviation of depth ratio
+#'   - CNt: integer, total copy number
+#'   - A: integer, copy number of allele A
+#'   - B: integer, copy number of allele B
+#'   - LPP: numeric, log posterior probability
+#' @param CCFs A numeric vector of possible CCF values.
+#' @param cellularity A numeric value representing the cellularity of the sample.
+#' @param ploidy A numeric value representing the ploidy of the sample.
+#' @param avg_depth A numeric value representing the average sequencing depth.
+#' @param CNn A numeric value representing the copy number in normal cells.
+#' @param avg.depth.ratio A numeric value representing the average depth ratio.
+#' @param CNst_diff A numeric value representing the difference in copy number states (default is 1).
+#' @param normal.ploidy A numeric value representing the normal ploidy (default is 2).
+#' @param ... Additional arguments (currently not used).
+#'
+#' @return A data frame with the original segment information and an additional column 'CCF' containing the calculated CCF values for each segment.
+#'
+#' @examples
+#' # Example usage:
+#' segs_tab <- data.frame(
+#'   chromosome = c(1, 1),
+#'   start.pos = c(10000, 20000),
+#'   end.pos = c(15000, 25000),
+#'   Bf = c(0.5, 0.6),
+#'   N.BAF = c(0.4, 0.5),
+#'   sd.BAF = c(0.01, 0.02),
+#'   depth.ratio = c(1.2, 1.3),
+#'   N.ratio = c(1.1, 1.2),
+#'   sd.ratio = c(0.05, 0.06),
+#'   CNt = c(2, 3),
+#'   A = c(1, 1),
+#'   B = c(1, 2),
+#'   LPP = c(0.9, 0.8)
+#' )
+#' CCFs <- seq(0, 1, by = 0.1)
+#' cellularity <- 0.8
+#' ploidy <- 2
+#' avg_depth <- 30
+#' CNn <- 2
+#' avg.depth.ratio <- 1.1
+#' result <- ccf_per_segment(segs_tab, CCFs, cellularity, ploidy, avg_depth, CNn, avg.depth.ratio)
+#' print(result)
+#'
+#' @export
+ccf_per_segment <- function(segs_tab, CCFs, cellularity, ploidy, avg_depth, CNn,
+    avg.depth.ratio, CNst_diff = 1, normal.ploidy = 2, ...) {
+
+    # Generate BAF/ratio model points
+    types_matrix <- baf.types.matrix(0, max(segs_tab$CNt) + CNst_diff, CNn)
+    types_values <- baf.model.points(cellularity, ploidy, types_matrix, avg.depth.ratio)
+    types_data <- cbind(types_matrix, types_values)
+
+    # Vectorized operations
+    CNt_values <- segs_tab$CNt
+    depth_ratios <- segs_tab$depth.ratio
+    depths <- avg_depth * depth_ratios
+
+    # Get type ratios for each CNt value
+    type_ratios <- sapply(CNt_values, function(cnt) {
+        ratios <- types_data$depth.ratio[types_data$CNt == cnt]
+        if (length(ratios) > 0)
+            ratios[1] else NA
+    })
+
+    # Calculate subclonal CNt values
+    i_sCNt <- ifelse(type_ratios >= depth_ratios, CNt_values - CNst_diff, CNt_values +
+        CNst_diff)
+
+    # Calculate CCF likelihoods for all segments
+    ccf_results <- t(mapply(function(ratio, cnt, scnt, depth) {
+        likelihood <- depth_ratio_ccf_likelihood(x = ratio, CCFs = CCFs, CNn = CNn,
+            CNt = cnt, CNst = scnt, cellularity = cellularity, ploidy = ploidy, depth = depth,
+            avg.depth.ratio = avg.depth.ratio, normal.ploidy = normal.ploidy)
+        c(CCF = CCFs[which.max(likelihood)], CNst = scnt, llp = -log(max(likelihood)))
+    }, depth_ratios, CNt_values, i_sCNt, depths))
+
+    return(ccf_results)
+}
+
 # Example usage for CCF likelihood plot: plot( seq(0, 1, 0.001),
 # depth_ratio_ccf_likelihood( x = 2.2, CCFs = seq(0, 1, 0.001), CNn = 2, CNt =
 # 4, CNst = 5, cellularity = 1, ploidy = 2, depth = 100 ), type = 'l' )
